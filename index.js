edd575ef9452a796b81dbe7ae615b3e4a8ecb3b6
Shreyas Gupta <technology.shreyas@gmail.com>
2021-09-05 20:19:26 +0530

    add download movie template

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+})
+
+app.get('/movie', (req, res) => {
+  res.sendFile(path.join(__dirname+'/views/moviesingle.html'));
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Baiting app listening at http://localhost:${port}`)
 })
